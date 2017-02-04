0cbd5497d1dbd17dbfc1d50e705bbdc7b72c0f4c
Maksim Nikolskiy <nikolskijmaksim@gmail.com>
2017-02-04 16:27:32 +0300

    finite-state-machine: implemented, tests passed

--- src/fsm.js
+++ src/fsm.js
@@ -3,30 +3,63 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+        if (!config) {
+            throw new Error();
+        }
+
+        this.config = config;
+        this.currentState = config.initial;
+        this.undoHistory = [];
+        this.redoHistory = [];
+
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return this.currentState;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
-
+    changeState(state) {
+ 
+        if (this.config.states[state]) {
+            this.undoHistory.unshift(this.currentState);
+            this.redoHistory.length = 0;
+            this.currentState = state;
+        }
+        else {
+            throw new Error();
+        }   
+     }
+       
     /**
      * Changes state according to event transition rules.
      * @param event
      */
-    trigger(event) {}
+    trigger(event) {
+       
+        if (this.config.states[this.currentState].transitions[event]) {
+            this.redoHistory.length = 0;
+            this.changeState(this.config.states[this.currentState].transitions[event]);
+        }
+        else {
+            throw new Error();
+        }
+    }
 
     /**
      * Resets FSM state to initial.
      */
-    reset() {}
+    reset() {
+        this.currentState = this.config.initial;
+    }
 
     /**
      * Returns an array of states for which there are specified event transition rules.
@@ -34,26 +67,62 @@ class FSM {
      * @param event
      * @returns {Array}
      */
-    getStates(event) {}
+    getStates(event) {
+        var statesAray = [];
+        var states=this.config.states;
+
+        if (!event) {
+            return statesAray = Object.keys(states);
+        }
+        else {
+            for (var state in states) {
+                if (Object.keys(states[state].transitions).includes(event)) {
+                    statesAray.push(state);
+                }
+            }
+            return statesAray;
+        }
+    }
 
     /**
      * Goes back to previous state.
      * Returns false if undo is not available.
      * @returns {Boolean}
      */
-    undo() {}
+    undo() {
+        if (this.undoHistory.length) {
+            this.redoHistory.unshift(this.currentState);
+            this.currentState = this.undoHistory.shift();
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
     /**
      * Goes redo to state.
      * Returns false if redo is not available.
      * @returns {Boolean}
      */
-    redo() {}
+    redo() {
+        if (this.redoHistory.length) {
+            this.undoHistory.unshift(this.currentState);
+            this.currentState=this.redoHistory.shift();
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
 
     /**
      * Clears transition history
      */
-    clearHistory() {}
+    clearHistory() {
+        this.undoHistory.length = 0;
+        this.redoHistory.length = 0;
+    }
 }
 
 module.exports = FSM;
